55a0721b88c63ea10e99da35ed3a7a1526ab0d67
TOGHoney <honey.gn8514@gmail.com>
2026-09-11 13:21:46 +0530

    Predefined Exception

---
+++ PredefinedException.java
@@ -0,0 +1,24 @@
+import java.io.FileReader;
+import java.util.Scanner;
+public class PredefinedException{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        try{
+            int[] arr = {1, 2, 3, 4};
+            int i ,b;
+            FileReader fr = new FileReader("abc.text");
+            i = sc.nextInt();
+            b = sc.nextInt();
+            System.out.println(arr[i]);
+            System.out.println(arr[i]/b);
+        }catch(FileNotFoundException e){
+            System.out.println("Exception caught:" + e.getMessage());
+        }catch(ArithematicException e){
+            System.out.println("Exception caught:"+e.getMessage());
+        }catch(ArrayIndexOutOfBoundException e){
+            System.out.println("Exception caught: "+e.getMessage());
+        }finally{
+            System.out.println("program ended");
+        }
+    }
+}
