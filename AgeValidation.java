06ba4e6ac6e7e74786f6cf036cd77bdf3e9bdb48
TOGHoney <honey.gn8514@gmail.com>
2026-09-12 11:43:46 +0530

    AGE VALIDATION

--- AgeValidation.java
+++ AgeValidation.java
@@ -4,14 +4,14 @@ public class AgeValidation{
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
         try{
-            checkAge(a);
+            checkAge(age);
         }catch(AgeInvalidException e){
-            System.out.prinln(e);
+            System.out.println(e);
         }finally{
             System.out.println("Program ended");
         }
     }
-    void checkAge(int age) throws AgeInvalideException {
+    static void checkAge(int age) throws AgeInvalidException {
         if(age<18) throw new AgeInvalidException("Ageis not valid to vote");
         System.out.println("Eligible to vote");
     }
