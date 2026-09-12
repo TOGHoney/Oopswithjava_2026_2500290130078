83b718bfc4a1f11b6155e3db01352d02bc1a6374
TOGHoney <honey.gn8514@gmail.com>
2026-09-12 12:04:30 +0530

    EXCEPTION

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 public class ExceptionHandling{
-    Scanner obj = new Scanner(System.in);
-    int arr[] = {1, 2, 3};
-    int i = obj.nextInt();
-    try{
-        System.out.println(arr[i]);
-        int a = obj.nextInt();
-        System.out.prinln(arr[i]/a);
-    }catch(Exception a){
-        a.printStackTrace();
-    } finally {
-        System.out.println("program finished");
+    public static void main(String[] args){
+        Scanner obj = new Scanner(System.in);
+        int arr[] = {1, 2, 3};
+        int i = obj.nextInt();
+        try{
+            System.out.println(arr[i]);
+            int a = obj.nextInt();
+            System.out.println(arr[i]/a);
+        }catch(Exception a){
+            a.printStackTrace();
+        } finally {
+            System.out.println("program finished");
+        }
+        obj.close();
     }
 }
